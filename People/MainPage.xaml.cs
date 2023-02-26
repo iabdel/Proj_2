48d3ed67cc01012ae1d17d96c7d8841ec3b71bd9
iabdel <isam.abdelhameed@outlook.com>
2023-02-26 18:26:27 +0300

    wip

---
+++ People/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using People.Models;
+using System.Collections.Generic;
+
+namespace People;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    public async Task OnNewButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        await App.PersonRepo.AddNewPerson(newPerson.Text);
+        statusMessage.Text = App.PersonRepo.StatusMessage;
+    }
+
+    public async Task OnGetButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        List<Person> people = await App.PersonRepo.GetAllPeople();
+        peopleList.ItemsSource = people;
+    }
+
+}
+
